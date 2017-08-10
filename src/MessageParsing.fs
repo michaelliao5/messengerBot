b9dbc9a0fde759790bc9f0dfd3e23cbfa7e4bddc
michael.liao <michael.liao@jet.com>
2017-08-10 09:48:33 -0400

    added isbn and pricing, TODO put them in one function

--- src/MessageParsing.fs
+++ src/MessageParsing.fs
@@ -21,26 +21,50 @@ module MessageParsing =
         if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
         else None
 
-  let booktitleRun (bookTitle:string) : string= 
+  let booktitleRun (bookTitle:string) : string*string = 
     url (sprintf "https://jet.com/search?term=%s" <| bookTitle.Replace(" ", "%20"))
     click ".products-right"
-    let price = read ".*-size-medium.a-color-price.header-price"
+    let price = read ".formatted-value"
     let isbn = elementWithText ".h5" "ISBN13"
     let ss = read isbn
     let s = ss.Split([|' ';'\n'|]).[1]
-    s
+    (s,price)
+    
+  let amazonPriceRun (isbn:string) : string = 
+    url <| "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=" + isbn
+    click ".s-access-image.cfMarker"
+    let price = read ".*-size-medium.a-color-price"
+    price
 
-  let getISBN (bookTitle:string) : string = 
+
+  let getISBN (bookTitle:string) : string*string = 
     start chrome
-    let s = booktitleRun(bookTitle)
+    let (s,price) = booktitleRun(bookTitle)
     quit()
-    s
+    (s,price)
+
+  let getAmazonPrice (isbn:string) : string = 
+    start chrome
+    let price = amazonPriceRun(isbn)
+    quit()
+    price
+    
 
   let ProcessMessage (input:string) : Message = 
     match input with    
     | Regex @"look up book (.*)" [ bookTitle ] ->
-        let isbn = getISBN bookTitle
-
+        let (isbn,jetPrice) = getISBN bookTitle
+        let amazonPrice = getAmazonPrice isbn
+        let msg = sprintf "Price on jet: %s Price on amazon:%s" jetPrice amazonPrice
         Message.Text(isbn, None, None)
     | _ -> Message.Text(apiAi.TextRequest(input).Result.Fulfillment.Speech, None, None)
+
+  let ProcessMessage2 (input:string) : Message = 
+//    match input with    
+//    | Regex @"look up book (.*)" [ bookTitle ] ->
+//        let isbn = getISBN bookTitle
+//
+//        Message.Text(isbn, None, None)
+//    | _ -> 
+    Message.Text(apiAi.TextRequest(input).Result.Fulfillment.Speech, None, None)
 
